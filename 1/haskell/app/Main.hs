92e5c6aec0c55a366617a38eee20a3440dd9e61d
Roberto Paredes <rdparedessalazar@gmail.com>
2022-12-12 09:07:55 +0100

    Day 1 - second part - Haskell

--- 1/haskell/app/Main.hs
+++ 1/haskell/app/Main.hs
@@ -2,7 +2,8 @@
 
 module Main where
 
-import System.IO ( IOMode(ReadMode), hGetContents, openFile )
+import Data.List (sort)
+import System.IO (IOMode (ReadMode), hGetContents, openFile)
 
 main :: IO ()
 main = do
@@ -10,23 +11,29 @@ main = do
   contents <- hGetContents handle
   -- split the contents into lines
   let c = lines contents
-  let accums = (0, 0)
-  let final_accums = reduce c accums
+  -- Use the first element in the tuple to store the top 3 biggest groups. Use the
+  -- second element as temporary storage to add each element of a group.
+  let accumulators = ([0, 0, 0], 0)
+  let final_accums = reduce c accumulators
   -- print the biggest number
-  print final_accums
+  print (total final_accums)
+  where
+    total (top_three, _) = sum top_three
 
 -- Define a function that takes two accumulators and a value from the input list,
 -- and returns a pair containing the updated accumulator values.
-combine :: (Int, Int) -> String -> (Int, Int)
-combine (biggest, temp) c = (biggest', temp')
+combine :: ([Int], Int) -> String -> ([Int], Int)
+combine (top_three, temp) c = (top_three', temp')
   where
-    biggest' = if c == "" then max biggest temp else biggest
+    top_three' = if c == "" then pick_highest top_three temp else top_three
+      where
+        pick_highest xs x = take 3 . reverse . sort $ x : xs
     temp' = if c == "" then 0 else temp + readInt c
+      where
+        readInt :: String -> Int
+        readInt = read
 
 -- Define the reduce function, which uses foldl to apply the combine function
 -- to each value in the input list, using the given initial accumulator values.
-reduce :: [String] -> (Int, Int) -> (Int, Int)
-reduce xs (biggest, temp) = foldl combine (biggest, temp) xs
-
-readInt :: String -> Int
-readInt = read
+reduce :: [String] -> ([Int], Int) -> ([Int], Int)
+reduce xs (top_three, temp) = foldl combine (top_three, temp) xs
