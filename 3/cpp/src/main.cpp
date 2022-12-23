0b002b24d6e6de7bd27c330242650f9bc5b3d3e6
Roberto Paredes <rdparedessalazar@gmail.com>
2022-12-23 08:13:43 +0100

    Day 3 - Second part - C++

--- 3/cpp/src/main.cpp
+++ 3/cpp/src/main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <array>
+#include <unordered_map>
 
 /*
 The first half of the characters represent items in the first compartment,
@@ -15,6 +16,14 @@ Lowercase item types a through z have priorities 1 through 26.
 Uppercase item types A through Z have priorities 27 through 52.
 
 Find the item type that appears in both compartments of each rucksack.
+What is the sum of the priorities of those item types?
+
+PART 2:
+
+The only way to tell which item type is the right one is by finding the one item type that
+is common between all three Elves in each group.
+
+Find the item type that corresponds to the badges of each three-Elf group.
 What is the sum of the priorities of those item types?
  */
 
@@ -65,12 +74,38 @@ const char find_duplicate(const std::array<string, 2> &compartments)
       }
     }
   }
-  return NULL;
+  return '\0';
+}
+
+const char find_badge(const std::array<string, 3> &groups)
+{
+  std::unordered_map<char, int> char_counts;
+  for (char c : groups[0])
+  {
+    char_counts[c] = 1;
+  }
+  for (char c : groups[1])
+  {
+    if (char_counts[c])
+    {
+      char_counts[c] = 2;
+    }
+  }
+  for (char c : groups[2])
+  {
+    if (char_counts[c] > 1)
+    {
+      cout << "found this letter! -> " << c << endl;
+      return c;
+    }
+  }
+  return '\0';
 }
 
 int main(int argc, char **argv)
 {
   string buffer;
+  std::array<string, 3> current_group = {"", "", ""};
   int total = 0;
   std::ifstream ifs;
   ifs.open("./input.txt", std::ios::in);
@@ -79,8 +114,14 @@ int main(int argc, char **argv)
   {
     while (!ifs.eof())
     {
-      std::getline(ifs, buffer);
-      const int item_priority = get_item_priority(find_duplicate(split_sack(buffer)));
+      int temp = 0;
+      while (temp < 3)
+      {
+        std::getline(ifs, buffer);
+        current_group[temp] = buffer;
+        temp++;
+      }
+      const int item_priority = get_item_priority(find_badge(current_group));
       total += item_priority;
     }
   }
