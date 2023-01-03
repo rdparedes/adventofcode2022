0184f6232fa4b6303b3457ceaebe79cf2cdb104a
Roberto Paredes <rdparedessalazar@gmail.com>
2023-01-03 19:42:14 +0100

    Day 4 - Second part - Haskell

--- 4/haskell/app/Main.hs
+++ 4/haskell/app/Main.hs
@@ -1,11 +1,16 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
 {-
 In how many assignment pairs does one range fully contain the other?
 For example, 2-8 fully contains 3-7, and 6-6 is fully contained by 4-6.
+
+--- Part Two ---
+
+In how many assignment pairs do the ranges overlap?
  -}
 
 module Main where
 
-import Data.List (break)
 import Data.List.Split (splitOn)
 import System.IO (IOMode (ReadMode), hGetContents, openFile)
 
@@ -14,11 +19,20 @@ main = do
   handle <- openFile "input.txt" ReadMode
   contents <- hGetContents handle
   let entries = lines contents
-  let total = foldr (\entry acc -> acc + aContainsB entry) 0 entries
-  print total
+  let totalContains = foldr (\entry acc -> acc + aContainsB entry) 0 entries
+  let totalOverlaps = foldr (\entry acc -> acc + aOverlapsWithB entry) 0 entries
+  print ("Total contains: ", totalContains)
+  print ("Total overlaps: ", totalOverlaps)
 
 aContainsB :: String -> Int
 aContainsB entry = fromEnum (contains a b || contains b a)
+  where
+    [strA, strB] = splitOn "," entry
+    a = assignmentFromString strA
+    b = assignmentFromString strB
+
+aOverlapsWithB :: String -> Int
+aOverlapsWithB entry = fromEnum (overlaps a b)
   where
     [strA, strB] = splitOn "," entry
     a = assignmentFromString strA
@@ -40,3 +54,7 @@ assignmentFromString str =
 contains :: Assignment -> Assignment -> Bool
 contains a b =
   (firstElem a <= firstElem b) && (lastElem a >= lastElem b)
+
+overlaps :: Assignment -> Assignment -> Bool
+overlaps a b =
+  (lastElem b >= firstElem a) && (firstElem b <= lastElem a)
