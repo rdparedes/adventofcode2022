5f08f81cfb6630c59051302c54b3c61af7663b1c
Roberto Paredes <rdparedessalazar@gmail.com>
2023-01-03 17:42:35 +0100

    Day 4 - First part - Haskell

---
+++ 4/haskell/app/Main.hs
@@ -0,0 +1,42 @@
+{-
+In how many assignment pairs does one range fully contain the other?
+For example, 2-8 fully contains 3-7, and 6-6 is fully contained by 4-6.
+ -}
+
+module Main where
+
+import Data.List (break)
+import Data.List.Split (splitOn)
+import System.IO (IOMode (ReadMode), hGetContents, openFile)
+
+main :: IO ()
+main = do
+  handle <- openFile "input.txt" ReadMode
+  contents <- hGetContents handle
+  let entries = lines contents
+  let total = foldr (\entry acc -> acc + aContainsB entry) 0 entries
+  print total
+
+aContainsB :: String -> Int
+aContainsB entry = fromEnum (contains a b || contains b a)
+  where
+    [strA, strB] = splitOn "," entry
+    a = assignmentFromString strA
+    b = assignmentFromString strB
+
+data Assignment = Assignment
+  { firstElem :: Int,
+    lastElem :: Int
+  }
+  deriving (Show)
+
+assignmentFromString :: String -> Assignment
+assignmentFromString str =
+  let (sFirst, sLast) = span (/= '-') str
+      firstElem = read sFirst
+      lastElem = read (drop 1 sLast)
+   in Assignment {firstElem, lastElem}
+
+contains :: Assignment -> Assignment -> Bool
+contains a b =
+  (firstElem a <= firstElem b) && (lastElem a >= lastElem b)
